91b041dd7f649133a0f9db9f2f0329b8a40c94d5
hasegawa-takashi <kamikirihien@gmail.com>
2017-02-25 21:01:35 +0900

    不要なファイルの設定。Fade周りを組みなおし

--- DireectX_00/DxDevice.cpp
+++ DireectX_00/DxDevice.cpp
@@ -118,4 +118,5 @@ bool CDxDevice::CreateDxDevice()
 		return false;
 	}
 
+	return true;
 }
--- data/cri/common/license/print_hostid.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem ライセンスキーファイル申請用ホストIDの表示
-
-lmhostid | %SystemRoot%\system32\find "FLEXnet" >> %TMP%\temp_ipconfig.txt
-%SystemRoot%\system32\notepad %TMP%\temp_ipconfig.txt
-del %TMP%\temp_ipconfig.txt
