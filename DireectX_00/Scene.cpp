97e7340661257387c905081864452908659cf986
HasegawaTakashi <kamikirihien@gmail.com>
2017-03-04 03:12:12 +0900

    Scene.cppの入れ忘れ

---
+++ DireectX_00/Scene.cpp
@@ -0,0 +1,113 @@
+#include "Scene.h"
+
+//-------------------------------------------------------
+//
+//	シーンの新規挿入
+//
+//-------------------------------------------------------
+bool CSceneMgr::CreateScene() {
+	auto pCreateFunc = m_SceneCreatorFunctionList.back();
+	m_SceneCreatorFunctionList.pop_back();
+	m_pCurrentScene = pCreateFunc();
+	return true;
+}
+
+//-------------------------------------------------------
+//
+//	シーンの削除
+//
+//-------------------------------------------------------
+bool CSceneMgr::DeleteScene() {
+	SAFE_DELETE(m_pCurrentScene);
+	return true;
+}
+
+//-------------------------------------------------------
+//
+//	シーンの交換
+//
+//-------------------------------------------------------
+bool CSceneMgr::ChangeScene(/*CSceneBase* scene*/) {
+	// 次のシーンが設定されていない
+	if (m_SceneCreatorFunctionList.empty()) return false;
+
+	// シーンをまたいで保持したいオブジェクトの取得
+	// 遷移のタイミングで削除するため
+	std::list<ObjBase*> ExcludeObjList = GetObjMgr()->ExculdeObj();
+
+	// シーンの削除
+	if (!DeleteScene()) return false;
+
+	// シーンの挿入
+	if (!CreateScene()) return false;
+
+	// シーンの初期化
+	for (auto& pObj : ExcludeObjList)
+	{
+		GetObjMgr()->PushObj(pObj, pObj->GetIDNumb());
+	}
+	m_pCurrentScene->Init();
+
+	return true;
+}
+
+//-------------------------------------------------------
+//
+//	次のSceneを設定
+//
+//-------------------------------------------------------
+template <typename T>
+void CSceneMgr::PushNextScne(CSceneBase* scene)
+{
+	// 次のシーンの登録
+	m_NextScene = scene;
+}
+
+
+//-------------------------------------------------------
+//
+//	登録シーンの更新
+//
+//-------------------------------------------------------
+void CSceneMgr::Update() {
+	if (m_pCurrentScene == nullptr) return;
+	m_pCurrentScene->Update();
+	m_pCurrentScene->LateUpdate();
+}
+
+//-------------------------------------------------------
+//
+//	登録シーン描画
+//
+//-------------------------------------------------------
+void CSceneMgr::Draw() {
+	if (m_pCurrentScene == nullptr) return;
+	HRESULT hr = GetDxMgr()->GetDxDevice()->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER | D3DCLEAR_STENCIL, D3DCOLOR_XRGB(128, 128, 255), 1.0f, 0);
+
+	if (SUCCEEDED(GetDxMgr()->GetDxDevice()->BeginScene()))
+	{
+		// 通常描画
+		m_pCurrentScene->Draw();
+		// 透過処理
+		GetRenderMgr()->RenderTransStart();
+		// 後からの描画
+		m_pCurrentScene->LateDraw();
+		// UI等の最後の描画
+		m_pCurrentScene->UIDraw();
+		// 描画の終了
+		GetRenderMgr()->RenderEnd();
+
+	}
+
+	CDebug::Instance()->Render();
+	GetDxMgr()->GetDxDevice()->Present(NULL, NULL, NULL, NULL);
+}
+
+//-------------------------------------------------------
+//
+//	シーンmgrの終了処理
+//
+//-------------------------------------------------------
+void CSceneMgr::Release() {
+	SAFE_DELETE(m_pCurrentScene);
+}
