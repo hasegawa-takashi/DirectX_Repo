39644d7237f2a89572f717b32d3beb7fc2cd07e8
hasegawa-takashi <kamikirihien@gmail.com>
2017-04-12 01:12:35 +0900

    Wavファイルの読み込みの作成_XAudio2への入れ方がわからないのが悩み

--- DireectX_00/SoundMgr.cpp
+++ DireectX_00/SoundMgr.cpp
@@ -4,9 +4,67 @@
 
 CSoundMgr::CSoundMgr()
 {
+	m_wfx = {0};
+	CreateXAudio();
 }
 
 
 CSoundMgr::~CSoundMgr()
 {
+}
+
+//==========================================================
+//
+//		XAudio2の初期化
+//
+bool CSoundMgr::CreateXAudio()
+{
+
+	UINT32 flags = 0;
+	m_pMasterVoice = NULL;
+
+
+	if (FAILED(hr = CoInitializeEx(NULL, COINIT_MULTITHREADED)))
+	{
+		return false;
+	}
+
+	// XAudioの初期化
+	if (FAILED(hr = XAudio2Create(&m_pXAudio, flags)))
+	{
+		return false;
+	}
+
+
+	// 使用可能オーディオデバイス数
+	m_pXAudio->GetDeviceCount(&m_devicecnt);
+
+	// デバイスの選択
+	XAUDIO2_DEVICE_DETAILS devicedetails;
+	int preferredDevice = 0;
+
+	for (UINT loop = 0;loop < m_devicecnt ; loop++)
+	{
+		m_pXAudio->GetDeviceDetails(loop, &devicedetails);
+		if (devicedetails.OutputFormat.Format.nChannels > 2)
+		{
+			preferredDevice = loop;
+		}
+	}
+
+	// マスターボイスの作成
+	if (FAILED(hr = m_pXAudio->CreateMasteringVoice(&m_pMasterVoice, XAUDIO2_DEFAULT_CHANNELS, XAUDIO2_DEFAULT_SAMPLERATE, 0, preferredDevice, NULL)))
+	{
+		return false;
+	}
+}
+
+//==========================================================
+//
+//		再生準備
+//
+void CSoundMgr::FormatWaveTex()
+{
+
+
 }
--- DireectX_00/SoundMgr.h
+++ DireectX_00/SoundMgr.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<XAudio2.h>
 
 class CSoundMgr
 {
@@ -8,6 +9,19 @@ public:
 	~CSoundMgr();
 
 private:
+	bool CreateXAudio();
+	void FormatWaveTex();
+
+	
+private: // ïœêî
+	IXAudio2* m_pXAudio;
+	HRESULT hr;
+	IXAudio2MasteringVoice* m_pMasterVoice;
+	IXAudio2SourceVoice* Voice;
+	UINT32 m_devicecnt;
+
+	WAVEFORMATEX m_wfx;
+
 
 };
 
